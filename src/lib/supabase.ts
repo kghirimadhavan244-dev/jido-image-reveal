bf3d2815fe7810cc0e3a21b926d3344c73de01de
Ghiri Madhavan <bl.en.u4elc23010@blr.students.amrita.edu>
2026-07-27 01:07:16 +0530

    Describe your changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY exists:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export const BUCKET_NAME = "images";
-export const GAMES_TABLE = "games";
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
